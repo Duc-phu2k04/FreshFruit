28c86d8487b9a5785c8323fa1666f874d0f396e4
thanh-31032004 <vanthanhnguyen3103@gmail.com>
2025-06-14 08:19:17 +0700

    add Checkout UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Layout from "./layouts/Main-layout"
 import Homepage from "./pages/Homepage/Homepage"
+import Checkout from "./pages/Checkout/Checkout"
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Homepage />} />
+          <Route path="/checkout" element={<Checkout />} />
         </Route>
       </Routes>
     </BrowserRouter >
