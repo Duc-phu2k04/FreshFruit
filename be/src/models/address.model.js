2f574bc587eea8936d7ea31fbdcf97257e29bf04
Duc-phu2k04 <nguyenducphu2k4@gmail.com>
2025-08-03 02:34:36 +0700

    add file addressship

---
+++ be/src/models/address.model.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+const addressSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    province: {
+      type: String,
+      required: true,
+    },
+    district: {
+      type: String,
+      required: true,
+    },
+    ward: {
+      type: String,
+      required: true,
+    },
+    detail: {
+      type: String,
+      required: true,
+    },
+    isDefault: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('Address', addressSchema);
